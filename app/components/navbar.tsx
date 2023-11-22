59a91e034d3889e35aedcbacd0fb6aabcd8128b6
MarcusMarcusMarcusMarcusMarcus <marcus@ilder.no>
2023-11-22 20:01:32 +0100

    c

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -4,15 +4,15 @@ import Image from "next/image";
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="p-0 w-full z-10 fixed top-0 ml-16"> {/* Increase the left margin to move the entire Navbar more to the right */}
-      <div className="container mx-auto h-20 flex items-center justify-center"> {/* Center the content */}
-        <div className="text-white font-bold text-lg">
-          <a href="#">
-            <Image src={logo} alt="Marcus Skålevik" width={200} height={200} />
-          </a>
-        </div>
-        <div className="flex ml-4 space-x-4"> {/* Add ml-4 to create space between logo and subpages */}
-          {/* <a href="#" className="text-education hover:text-black">About</a> */}
+    <nav className="p-0 fixed top-0 w-full z-10">
+      <div className="container mx-auto">
+        <div className="flex items-center justify-center"> {/* Center the content */}
+          <div className="text-white font-bold text-lg ">
+            <a href="#">
+              <Image src={logo} alt="Marcus Skålevik" width={200} height={200} />
+            </a>
+          </div>
+          {/* Add space-x and links here if needed */}
         </div>
       </div>
     </nav>
